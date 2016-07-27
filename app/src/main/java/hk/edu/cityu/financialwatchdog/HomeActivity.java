82231d729586bb44f311a38bd1dca9add572c8f2
Milan <milankostak@seznam.cz>
2016-07-27 10:07:07 +0800

    PieChart applied

--- app/src/main/java/hk/edu/cityu/financialwatchdog/HomeActivity.java
+++ app/src/main/java/hk/edu/cityu/financialwatchdog/HomeActivity.java
@@ -9,6 +9,15 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +35,38 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        initPieChart();
+    }
+
+    private void initPieChart() {
+        PieChart pieChart = (PieChart) findViewById(R.id.testChart);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(12f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(90f, 5));
+
+        PieDataSet dataset = new PieDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        PieData data = new PieData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        pieChart.setDescription("Description");
+        pieChart.setData(data);
+
+        pieChart.animateY(5000);
+
+        pieChart.saveToGallery("/sd/mychart.jpg", 85); // 85 is the quality of the image
     }
 
     @Override
