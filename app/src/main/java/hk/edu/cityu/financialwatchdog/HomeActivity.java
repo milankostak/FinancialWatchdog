3369d3664d133dead47d000570e16b8703d4033b
Milan <milankostak@seznam.cz>
2016-07-31 15:29:09 +0800

    Setting refactor, opening on app bar

--- app/src/main/java/hk/edu/cityu/financialwatchdog/HomeActivity.java
+++ app/src/main/java/hk/edu/cityu/financialwatchdog/HomeActivity.java
@@ -134,7 +134,9 @@ public class HomeActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent i = new Intent(this, SettingActivity.class);
+            startActivity(i);
+            //return true;
         }
 
         return super.onOptionsItemSelected(item);
