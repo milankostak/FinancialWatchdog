1ede2796777e29576e9d648f441cfa73e6307a82
Milan <milankostak@seznam.cz>
2016-08-03 00:12:22 +0800

    fixed yesterday pie chart

--- app/src/main/java/hk/edu/cityu/financialwatchdog/fragments/PageFragmentYesterday.java
+++ app/src/main/java/hk/edu/cityu/financialwatchdog/fragments/PageFragmentYesterday.java
@@ -46,7 +46,7 @@ public class PageFragmentYesterday extends Fragment {
         cal1.set(Calendar.SECOND, 0);
         Calendar cal2 = Calendar.getInstance();
         cal2.setTime(new Date());
-        cal1.add(Calendar.DAY_OF_YEAR, -1);
+        cal2.add(Calendar.DAY_OF_YEAR, -1);
         cal2.set(Calendar.HOUR_OF_DAY, 23);
         cal2.set(Calendar.MINUTE, 59);
         cal2.set(Calendar.SECOND, 59);
